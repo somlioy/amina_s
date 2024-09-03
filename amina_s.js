e4a9b3bc443b70e1406be23023bc782e58371c1d
Vegard Sømliøy <somlioy@gmail.com>
2024-09-04 00:49:40 +0200

    More clean up and bug fixes from PR #12. Added control of new features. Scaled total and last sesion energy to kWh. Tested on Z2M 1.40.0

--- amina_s.js
+++ amina_s.js
@@ -1,9 +1,8 @@
 const {deviceAddCustomCluster, onOff, binary, numeric, enumLookup, electricityMeter} = require('zigbee-herdsman-converters/lib/modernExtend');
 const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
-const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const utils = require('zigbee-herdsman-converters/lib/utils');
-const ota = require('zigbee-herdsman-converters/lib/ota');
+const ota = require('zigbee-herdsman-converters/lib/ota')
 const e = exposes.presets;
 const ea = exposes.access;
 
@@ -13,14 +12,12 @@ const Amina_S_Control = {
     cluster: 0xFEE7,
     alarms: 0x02,
     ev_status: 0x03,
-    // Coming features start
     connect_status: 0x04,
     single_phase: 0x05,
     offline_current: 0x06,
     offline_single_phase: 0x07,
     time_to_offline: 0x08,
     enable_offline: 0x09,
-    // Coming features end
     total_active_energy: 0x10,
     last_session_energy: 0x11,
 }
@@ -56,9 +53,6 @@ const fzLocal = {
         type: ["attributeReport", "readResponse"],
         convert: (model, msg, publish, options, meta) => {
             const result = {};
-            if (msg.data.hasOwnProperty(Amina_S_Control.charge_limit_max)) {
-                result.charge_limit_max = msg.data[Amina_S_Control.charge_limit_max];
-            }
             
             if (msg.data.hasOwnProperty('alarms')) {
                 result.alarms = [];
@@ -78,16 +72,12 @@ const fzLocal = {
 };
 
 const tzLocal = {
-    amina_s: {
+    charge_limit: {
         key: ['charge_limit'],
         convertSet: async (entity, key, value, meta) => {
             const endpoint = entity.getDevice().getEndpoint(10);
-            switch(key) {
-                case 'charge_limit':
-                    const payload = {level: value, transtime: 0};
-                    await endpoint.command('genLevelCtrl', 'moveToLevel', payload, utils.getOptions(meta.mapped, entity));
-                    break;
-            }
+            const payload = {level: value, transtime: 0};
+            await endpoint.command('genLevelCtrl', 'moveToLevel', payload, utils.getOptions(meta.mapped, entity));
         },
         
         convertGet: async (entity, key, meta) => {
@@ -97,6 +87,12 @@ const tzLocal = {
     },
 
     // To be removed with introduction of threePhase electricalMeasurement modernExtend
+    total_active_power: {
+        key: ['total_active_power'],
+        convertGet: async (entity, key, meta) => {
+            await entity.read('haElectricalMeasurement', ['totalActivePower']);
+        },
+    },
     power_phase_b: {
         key: ['power_phase_b'],
         convertGet: async (entity, key, meta) => {
@@ -143,8 +139,9 @@ const definition = {
     description: 'Amina S EV Charger',
     ota: ota.zigbeeOTA,
     fromZigbee: [fzLocal.charge_limit, fz.electrical_measurement, fzLocal.amina_s],
-    toZigbee: [tzLocal.amina_s,
+    toZigbee: [tzLocal.charge_limit,
         // To be removed with introduction of threePhase electricalMeasurement modernExtend
+        tzLocal.total_active_power,
         tzLocal.power_phase_b, tzLocal.power_phase_c,
         tzLocal.accurrent_phase_b, tzLocal.accurrent_phase_c, 
         tzLocal.acvoltage_phase_b, tzLocal.acvoltage_phase_c,
@@ -169,6 +166,7 @@ const definition = {
             cluster: 'electrical',
             // threePhase: true,
         }),
+
         deviceAddCustomCluster(
             'aminaControlCluster',
             {
@@ -177,14 +175,12 @@ const definition = {
                 attributes: {
                     alarms: {ID: Amina_S_Control.alarms, type: DataType.bitmap16},
                     evStatus: {ID: Amina_S_Control.ev_status, type: DataType.bitmap16},
-                    // Coming features start
                     connectStatus: {ID: Amina_S_Control.connect_status, type: DataType.bitmap16},
                     singlePhase: {ID: Amina_S_Control.single_phase, type: DataType.uint8},
                     offlineCurrent: {ID: Amina_S_Control.offline_current, type: DataType.uint8},
                     offlineSinglePhase: {ID: Amina_S_Control.offline_single_phase, type: DataType.uint8},
                     timeToOffline: {ID: Amina_S_Control.time_to_offline, type: DataType.uint16},
                     enableOffline: {ID: Amina_S_Control.enable_offline, type: DataType.uint8},
-                    // Coming features end
                     totalActiveEnergy: {ID: Amina_S_Control.total_active_energy, type: DataType.uint32},
                     lastSessionEnergy: {ID: Amina_S_Control.last_session_energy, type: DataType.uint32},
                 },
@@ -203,6 +199,19 @@ const definition = {
             attribute: 'evStatus',
             lookup: {'Not Connected': 0, 'EV Connected': 1, 'Ready to Charge': 3, 'Charging': 7, 'Paused': 11},
             description: 'Current charging status',
+            reporting: {min: 0, max: '1_MINUTE', change: 1},
+            access: 'STATE_GET',
+        }),
+
+        numeric({
+            name: 'total_active_power',
+            cluster: 'haElectricalMeasurement',
+            attribute: 'totalActivePower',
+            description: 'Instantaneous measured total active power',
+            reporting: {min: 0, max: '1_MINUTE', change: 10},
+            unit: 'kW',
+            scale: 1000,
+            precision: 2,
             access: 'STATE_GET',
         }),
 
@@ -211,7 +220,10 @@ const definition = {
             cluster: 'aminaControlCluster',
             attribute: 'totalActiveEnergy',
             description: 'Sum of consumed energy',
-            unit: 'Wh',
+            reporting: {min: 0, max: '1_MINUTE', change: 10},
+            unit: 'kWh',
+            scale: 1000,
+            precision: 2,
             access: 'STATE_GET',
         }),
 
@@ -220,8 +232,65 @@ const definition = {
             cluster: 'aminaControlCluster',
             attribute: 'lastSessionEnergy',
             description: 'Sum of consumed energy last session',
-            unit: 'Wh',
+            reporting: {min: 0, max: '1_MINUTE', change: 10},
+            unit: 'kWh',
+            scale: 1000,
+            precision: 2,
             access: 'STATE_GET',
+        }),
+
+        binary({
+            name: 'single_phase',
+            cluster: 'aminaControlCluster',
+            attribute: 'singlePhase',
+            description: 'Enable single phase charging. A restart of charging is required for the change to take effect.',
+            valueOn: ['Enable', 1],
+            valueOff: ['Disable', 0],
+            entityCategory: 'config',
+        }),
+
+        binary({
+            name: 'enable_offline',
+            cluster: 'aminaControlCluster',
+            attribute: 'enableOffline',
+            description: 'Enable offline mode when connection to the network is lost',
+            valueOn: ['Enable', 1],
+            valueOff: ['Disable', 0],
+            entityCategory: 'config',
+        }),
+
+        numeric({
+            name: 'time_to_offline',
+            cluster: 'aminaControlCluster',
+            attribute: 'timeToOffline',
+            description: 'Time until charger will behave as offline after connection has been lost',
+            valueMin: 0,
+            valueMax: 60,
+            valueStep: 1,
+            unit: 's',
+            entityCategory: 'config',
+        }),
+
+        numeric({
+            name: 'offline_current',
+            cluster: 'aminaControlCluster',
+            attribute:  'offlineCurrent',
+            description: 'Maximum allowed amperage draw when device is offline',
+            valueMin: 6,
+            valueMax: 32,
+            valueStep: 1,
+            unit: 'A',
+            entityCategory: 'config',
+        }),
+
+        binary({
+            name: 'offline_single_phase',
+            cluster: 'aminaControlCluster',
+            attribute: 'offlineSinglePhase',
+            description: 'Use single phase charging when device is offline',
+            valueOn: ['Enable', 1],
+            valueOff: ['Disable', 0],
+            entityCategory: 'config',
         }),
     ],
 
@@ -236,13 +305,11 @@ const definition = {
                                                     Amina_S_Control.alarms,
                                                     Amina_S_Control.ev_status,
                                                     Amina_S_Control.connect_status,
-                                                    /* Future functions
                                                     Amina_S_Control.single_phase,
                                                     Amina_S_Control.offline_current,
                                                     Amina_S_Control.offline_single_phase,
                                                     Amina_S_Control.time_to_offline,
                                                     Amina_S_Control.enable_offline,
-                                                    */
                                                     Amina_S_Control.total_active_energy,
                                                     Amina_S_Control.last_session_energy
                                                     ]);
